50028bd068ecd90b0e147896ffabe5f03e72c2a3
Arkamor <m.aucher@live.fr>
2020-02-14 08:58:30 +0100

    some tb

--- src/UM/UM.vhd
+++ src/UM/UM.vhd
@@ -1,25 +1,18 @@
----------------------------------------------------------
--- VHDL top level for 8 bits processor's UM                  
--- by Martin AUCHER & Kevin PEREZ, 02/2020
---
--- Code used for 8 bits processor courses at ENSEIRB-MATMECA
----------------------------------------------------------
-
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
-use ieee.numeric_std.ALL;
 
 ENTITY ram IS
-    PORT(
-        clk    : IN  STD_LOGIC;
-        ce     : IN  STD_LOGIC;
-        
-        rw     : IN  STD_LOGIC;
-        enable : IN  STD_LOGIC;
+	PORT(
+		clk		 : IN  STD_LOGIC;
+		ce       : IN  STD_LOGIC;
+		
+		rw       : IN  STD_LOGIC;
+		enable   : IN  STD_LOGIC;
 
-        addr   : IN  STD_LOGIC_VECTOR(5 DOWNTO 0);
-        data   : INOUT  STD_LOGIC_VECTOR(7 DOWNTO 0)
-    );
+		addr	 : IN  STD_LOGIC_VECTOR(6 DOWNTO 0);
+		data_in	 : IN  STD_LOGIC_VECTOR(7 DOWNTO 0);
+		data_out : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
+	);
 END ram;
 
 ARCHITECTURE rtl OF ram IS
@@ -36,21 +29,19 @@ ARCHITECTURE rtl OF ram IS
         X"00", X"00", X"00", X"00", X"00", X"00", X"00", X"00", 
         X"00", X"00", X"00", X"00", X"00", X"00", X"00", X"00"
     );
-BEGIN
 
+BEGIN
     PROCESS(clk)
     BEGIN
         IF(clk'EVENT AND clk = '1') THEN
             IF(ce = '1') THEN
-                 addr_int <= to_integer(unsigned(addr));
-                IF(enable = '1') THEN
-                    IF(rw = '1') THEN
-                        ram(addr_int) <= data;
-                    ELSE
-                        data <= ram(addr_int);
-                    END IF;
+                IF(rw = '1') THEN
+                    ram(addr) <= data_in;
+                ELSIF(enable = '1') THEN
+                    data_out <= ram(addr_int);
                 END IF;
-            END IF;    
-        END IF;    
+                addr_int <= integer(addr);
+            END IF;
+        END IF;	
     END PROCESS;
 END rtl;
